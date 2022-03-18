53ac2e9736ab3e248045e83e9b55fc9b2bc8aacf
Ernesto Estrada <strada.ernesto14@gmail.com>
2022-03-18 13:41:18 -0600

    Add mutate method to factory

--- main.js
+++ main.js
@@ -15,6 +15,27 @@ const mockUpStrand = () => {
 
 //console.log(mockUpStrand());
 
+// Factory funcion
+
+const pAequorFactory = (number, arrDNA) => {
+  return {
+    specimenNum: number,
+    dna: arrDNA,
+    mutate() {
+      let baseToMutate = Math.floor(Math.random() * arrDNA.length);
+      let newBase = returnRandBase();
+      while (arrDNA[baseToMutate] === newBase) {
+        newBase = returnRandBase();
+      }
+      console.log('Base to mutate: ' + arrDNA[baseToMutate] + ' . DNA index: ' + baseToMutate + '\nNew base: ' + newBase);
+      this.dna[baseToMutate] = newBase;
+    }
+  };
+}
+
+//const p1 = pAequorFactory(1, mockUpStrand());
+//console.log(p1.mutate());
+
 
 
 
