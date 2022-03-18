d92ffcb9c4a7cfa77acde20ead0d7a40c2837915
Ernesto Estrada <strada.ernesto14@gmail.com>
2022-03-18 14:29:28 -0600

    Create 30 objects that will survive

--- main.js
+++ main.js
@@ -43,15 +43,43 @@ const pAequorFactory = (number, arrDNA) => {
       console.log(`The specimens share a ${((dnaInCommon / 15) * 100.).toFixed(2)}% of DNA.`);
       //console.log(this.dna);
       //console.log(pObj.dna);
+    },
+    willLikelySurvive() {
+      dnaToSurvive = 0;
+      this.dna.forEach(dna => {
+        if (dna === 'C' || dna === 'G') {
+          dnaToSurvive++;
+        }
+      });
+      let result = ((dnaToSurvive / 15) * 100).toFixed(2);
+      console.log('Chances to survive: ' + result);
+      if (result >= 60) {
+        return true
+      } else {
+        return false;
+      }
     }
   };
 }
 
-const p1 = pAequorFactory(1, mockUpStrand());
-const p2 = pAequorFactory(2, mockUpStrand());
+//const p1 = pAequorFactory(1, mockUpStrand());
+//const p2 = pAequorFactory(2, mockUpStrand());
 //console.log(p1.mutate());
+//console.log(p1.compareDNA(p2));
 
-console.log(p1.compareDNA(p2));
+//console.log(p1.willLikelySurvive());
+
+const batch = [];
+
+for (let i = 1; i <= 30; i++) {
+  let specimen = pAequorFactory(i, mockUpStrand());
+  while (!specimen.willLikelySurvive) {
+    specimen= pAequorFactory(i, mockUpStrand());
+  }
+  batch.push(specimen);
+}
+
+//console.log(batch);
 
 
 
